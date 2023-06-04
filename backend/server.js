578c600a22c0834f0c04de223880b0a44992ce78
Darren Cal <darrencal64@gmail.com>
2023-06-03 22:52:40 -0600

    Enable CORS on backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,11 @@ connectDB();
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+};
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
