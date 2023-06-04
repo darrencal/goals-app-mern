246ce859b47f03bac3c34d80be378d8d95a91e89
Darren Cal <darrencal64@gmail.com>
2023-06-03 22:50:29 -0600

    Implement register functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
 import './index.css';
+import 'react-toastify/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { FaUserPlus } from 'react-icons/fa';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +16,25 @@ export default function Register() {
 
   const { name, email, password, passwordConfirm } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useAppSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -20,8 +44,23 @@ export default function Register() {
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log({ formData });
+
+    if (password !== passwordConfirm) {
+      toast.error('Passwords do not match');
+    } else {
+      const registerData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(registerData));
+    }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
